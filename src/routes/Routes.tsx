45c3a8311f87b1d7751265b3d039979438d84710
Guilherme <guilherme_gbm@outlook.com>
2024-04-22 09:38:22 -0300

    feat: create HomeSeller page route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes as RRDRouter, Route } from "react-router-dom";
-import Home from "../pages/home/Home";
+import { BrowserRouter, Routes as RRDRouter, Route } from 'react-router-dom';
+import Home from '../pages/home/Home';
+import HomeSeller from '@/pages/homeSeller/HomeSeller';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <RRDRouter>
         <Route path="/" element={<Home />} />
+        <Route path="/acesso-vendedor" element={<HomeSeller />} />
       </RRDRouter>
     </BrowserRouter>
   );
