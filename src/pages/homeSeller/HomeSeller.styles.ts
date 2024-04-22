9e0b175dadf9d7321e409cd201664f5cb1f2bf02
Guilherme <guilherme_gbm@outlook.com>
2024-04-22 09:36:06 -0300

    feat(HomeSeller): create HomeSeller page

---
+++ src/pages/homeSeller/HomeSeller.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HomeSellerContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const Header = styled.header``;
---
+++ src/pages/homeSeller/HomeSeller.tsx
@@ -0,0 +1,12 @@
+import FormContent from './FormContent/FormContent';
+import HomeSellerHeader from './HomeSellerHeader/HomeSellerHeader';
+
+export default function HomeSeller() {
+  return (
+    <>
+      <HomeSellerHeader />
+
+      <FormContent />
+    </>
+  );
+}
