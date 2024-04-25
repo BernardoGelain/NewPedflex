edab41ced8cdeaeeea0d750dd37efa65f42d99c4
Guilherme <guilherme_gbm@outlook.com>
2024-04-25 10:49:02 -0300

    refactor(Teste): remove non-used vars

--- src/pages/Teste.tsx
+++ src/pages/Teste.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { useCalculateTest } from "@/hooks/useCalculateTest";
-import { useFetchPost, useFetchPosts } from "@/services/posts";
+import { useFetchPosts } from "@/services/posts";
 
 export function Teste() {
-  const [id, setId] = React.useState(null);
-
   const test = useCalculateTest();
 
   const { postsData } = useFetchPosts();
-  const { postData } = useFetchPost(id);
 
   return (
     <div>
       {" "}
       <ul style={{ listStyleType: "circle", listStyle: "inside" }}>
-        <li>
-          <b>{postData?.title ?? ""}</b>
-        </li>
         {postsData?.map((it) => (
           <li>{it.title}</li>
         ))}
