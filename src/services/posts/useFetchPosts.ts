c9ac7f354154d0a18d39e65f0303f8cac73af786
Guilherme <guilherme_gbm@outlook.com>
2024-04-29 09:26:13 -0300

    feat: create fetch example with filters options

--- src/services/posts/useFetchPosts.ts
+++ src/services/posts/useFetchPosts.ts
@@ -1,24 +1,39 @@
 import { fakeApi } from "@/config/api";
 import { PostType } from "@/models/Posts";
+import { makeParams } from "@/utils/makeParams";
 import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
-const fetchPosts = async (): Promise<PostType[]> => {
-  const res = await fakeApi.get(`/posts`);
+type FetchPostsFilters = Partial<{
+  postId: number;
+  search: string;
+}>;
+
+const fetchPosts = async (filters?: FetchPostsFilters): Promise<PostType[]> => {
+  const params = makeParams(filters);
+
+  const res = await fakeApi.get(`/comments${params}`);
 
   return res.data;
 };
 
-export const useFetchPosts = () => {
+export const useFetchPosts = (initialFilters: FetchPostsFilters) => {
+  const [filters, setFilters] =
+    React.useState<FetchPostsFilters>(initialFilters);
+
   const { data, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", filters],
+    queryFn: () => fetchPosts(filters),
     refetchOnWindowFocus: "always",
     staleTime: 1000 * 60,
-
     select: (data) => {
       return data.map((post) => post);
     },
   });
 
-  return { postsData: data, isPostsLoading: isLoading };
+  return {
+    postsData: data,
+    isPostsLoading: isLoading,
+    setFetchPostsFilters: setFilters,
+  };
 };
