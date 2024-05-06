eef171c0fa27a1c223cbde6e4ddf599a02b26296
Guilherme <guilherme_gbm@outlook.com>
2024-05-06 16:59:00 -0300

    feat: create Currency and Discount Formatters

---
+++ src/utils/validators.ts
@@ -0,0 +1,14 @@
+export const formatInputCurrency = (amount: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(amount);
+};
+
+export const formatDiscount = (amount: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'decimal',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+};
