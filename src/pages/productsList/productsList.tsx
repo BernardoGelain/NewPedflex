c15cff5e17fecde4dbd306bafdf460daa5621d1f
guilherme_gbm <guilherme_gbm@outlook.com>
2024-05-17 14:09:38 -0300

    feat(list): create new function to correlate list and cart infos

--- src/pages/productsList/productsList.tsx
+++ src/pages/productsList/productsList.tsx
@@ -1,11 +1,31 @@
+import React from 'react';
 import { HomePageButton } from '@/components/HomeButton/HomeButton';
 import { useFetchProductsList } from '@/services/productsList';
 import { ProductCard } from './productCard/productCard';
 import { useAppSelector } from '@/hooks/useReduxHooks';
+import { ProductType } from '@/models/ProductsList';
 
 export function ProductsList() {
   const { productsListData } = useFetchProductsList();
   const { produtosCarrinho } = useAppSelector((state) => state.cart);
+
+  const assembleList = (): ProductType[] => {
+    if (productsListData && productsListData.length > 0) {
+      return productsListData.map((it, i) => {
+        if (
+          produtosCarrinho[i] &&
+          it.cd_produto === produtosCarrinho[i].cd_produto
+        ) {
+          return {
+            ...it,
+            cartInfo: produtosCarrinho[i].cartInfo,
+          };
+        }
+        return it;
+      });
+    }
+    return productsListData ?? [];
+  };
 
   return (
     <div>
@@ -13,11 +33,11 @@ export function ProductsList() {
 
       <HomePageButton linkTo="/carrinho" title="CART" />
 
-      {productsListData?.map((product, i) => (
+      {assembleList().map((product, i) => (
         <ProductCard
           key={product.cd_produto}
           product={product}
-          cartInfo={produtosCarrinho[i].cartInfo}
+          cartInfo={product.cartInfo}
         />
       ))}
     </div>
