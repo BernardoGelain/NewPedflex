d829f800620a8b5395c37126dff1d58de2a9f7dd
Guilherme <guilherme_gbm@outlook.com>
2024-05-20 16:26:05 -0300

    refactor: fix header values

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,19 +1,15 @@
-import { useAppSelector } from "@/hooks/useReduxHooks";
-import { useFetchAppHeader } from "@/services/header";
+import { useCartInfo } from '@/hooks/useCartInfo';
+import { useFetchAppHeader } from '@/services/header';
 
 export function AppHeader() {
   const { headerData } = useFetchAppHeader();
-
-  const { produtosCarrinho } = useAppSelector((state) => state.cart);
-
-  const productsOnCard = produtosCarrinho.filter(
-    (it) => it.cartInfo.quantidade > 0
-  );
+  const { cartQuantity, cartTotalValue } = useCartInfo();
 
   return (
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
       <h4>HEADER</h4>
-      <h2>{productsOnCard.length}</h2>
+      <h4>{cartQuantity}</h4>
+      <h4>{cartTotalValue}</h4>
     </div>
   );
 }
