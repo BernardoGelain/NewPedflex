2ae756b1a9775450800b2f6279f95bd3df159ab3
bernardo damiani <bmdamiani17@gmail.com>
2024-05-20 11:48:08 -0300

    feat:change show number of products on cart on header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,19 @@
-import { useFetchAppHeader } from '@/services/header';
+import { useAppSelector } from "@/hooks/useReduxHooks";
+import { useFetchAppHeader } from "@/services/header";
 
 export function AppHeader() {
   const { headerData } = useFetchAppHeader();
 
+  const { produtosCarrinho } = useAppSelector((state) => state.cart);
+
+  const productsOnCard = produtosCarrinho.filter(
+    (it) => it.cartInfo.quantidade > 0
+  );
+
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
       <h4>HEADER</h4>
+      <h2>{productsOnCard.length}</h2>
     </div>
   );
 }
